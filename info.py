86ca27b62cb3df903c24c69c424c9fb1505ce6ac
AllOkOfficial <108711998+AllOkOfficial@users.noreply.github.com>
2022-10-16 03:21:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,10 +29,10 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/47ec05e89d83789067f66.jpg https://telegra.ph/file/470687bf8a302c64e75c1.jpg https://telegra.ph/file/78f2e55cb4cff909cfe40.jpg https://telegra.ph/file/a176b5927783e6603a5bb.jpg https://telegra.ph/file/c699a79616886d90c2a01.jpg https://telegra.ph/file/3f4c0d562aa66a9856c6b.jpg https://telegra.ph/file/918ab60116c57c11ffec8.jpg https://telegra.ph/file/90ab448fbb72d99d93bf6.jpg https://telegra.ph/file/4974a6c5c63bcc371a086.jpg https://telegra.ph/file/f35f36aa266acd1d0c5d2.jpg https://telegra.ph/file/e92f87cd1fc2fd828028f.jpg https://telegra.ph/file/d7e6dd46dfc6fb8e9f62c.jpg https://telegra.ph/file/a6ff7413ee1ea06257084.jpg https://telegra.ph/file/59044a16947232e39a876.jpg https://telegra.ph/file/e88d4a781e23a7efea9cd.jpg https://telegra.ph/file/f613c75db7d77b1a31d50.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1906155412').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
